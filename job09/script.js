fc0636d6eaa6817d423914e5c047a575403ea896
alexandre-daumail <alexandre.daumail@laplateforme.io>
2022-03-14 16:52:50 +0100

    job09

---
+++ job09/script.js
@@ -0,0 +1,52 @@
+'use strict';
+
+/* Créez une fonction “tri” qui prend en paramètres un tableau de nombres
+nommé “numbers” et une variable “order” qui contient “asc” ou “desc”. A
+l’aide d’un algorithme développé par vos soins, cette fonction
+doit trier le tableau dans l’ordre ascendant ou décroissant, selon le
+paramètre passé, puis retourner le tableau. */
+
+function tri(numbers, order) {
+    
+    if (order == "asc"){
+
+        for (let i = 0; i < numbers.length; i++){
+
+            for (let j = 0; j < numbers.length; j++) {
+
+                if (numbers[j] < numbers[j-1]){
+    
+                    let memoire = numbers[j-1];
+                    numbers[j-1] = numbers[j];
+                    numbers[j] = memoire;
+                }   
+
+            }
+            
+        }
+
+    } else if (order == "desc") {
+
+        for (let i = 0; i < numbers.length; i++){
+
+            for (let j = 0; j < numbers.length; j++) {
+
+                if (numbers[j] < numbers[j+1]){
+    
+                    let memoire = numbers[j+1];
+                    numbers[j+1] = numbers[j];
+                    numbers[j] = memoire;
+                }   
+                             
+            }
+            
+        }
+
+    }
+}
+
+let numbers = [6,1,5,3];
+
+tri(numbers, "desc");
+
+console.log(numbers);
