046de910aa7e38121537d0cb66835c96836f84bb
alexandre-daumail <alexandre.daumail@laplateforme.io>
2022-03-14 11:04:24 +0100

    job04

---
+++ job04/script.js
@@ -0,0 +1,19 @@
+//Permet d'afficher les erreurs 
+'use strict';
+
+/* Déclarez une fonction “bisextile” qui prend en paramètre une variable
+“année”. Si l’année est bisextile, la fonction retourne true, sinon elle
+retourne false. */
+
+function bisextile(année) {
+    var année;
+
+    if (année % 4 == 0) {
+        return true;    
+    } else {
+        return false;
+    }
+
+}
+
+console.log(bisextile(2024));
