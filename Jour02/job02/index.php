be07db6a691d1924f2375420d6c91f4c67a284a0
alexandre-daumail <alexandre.daumail@laplateforme.io>
2022-03-15 13:20:57 +0100

    job02

---
+++ Jour02/job02/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js" defer></script>
+    <title>Accueil</title>
+</head>
+
+<body>
+    
+    <article class="">L'important n'est pas la chute, mais l'atterrissage.</article>
+
+    <button id="button">Afficher/masquer article</button>
+    
+</body>
+
+</html>
