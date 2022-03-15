63a26bf66654650c1ae0c017c73b631809a8912f
alexandre-daumail <alexandre.daumail@laplateforme.io>
2022-03-15 19:09:03 +0100

    job04

--- Jour02/job04/index.php
+++ Jour02/job04/index.php
@@ -6,14 +6,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="script.js" defer></script>
-    <title>Jour 2 | Job 03</title>
+    <title>Jour 2 | Job 04</title>
 </head>
 
 <body>
     
-    <p id="compteur">0</p>
-
-    <button id="button">Afficher/masquer article</button>
+<textarea name="" id="keylogger" cols="30" rows="10"></textarea>
     
 </body>
 
--- Jour02/job05/index.php
+++ Jour02/job05/index.php
@@ -5,16 +5,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <script src="script.js" defer></script>
-    <title>Jour 2 | Job 03</title>
+    <title>Jour 02 | Job05</title>
 </head>
 
 <body>
     
-    <p id="compteur">0</p>
-
-    <button id="button">Afficher/masquer article</button>
-    
+    <footer></footer>
 </body>
 
 </html>
