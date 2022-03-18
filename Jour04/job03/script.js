55fd4389ced17a8bc565e970b7f612a6bf46b41e
alexandre-daumail <alexandre.daumail@laplateforme.io>
2022-03-18 18:20:00 +0100

    select type pokemon
    script.js fetch all different types of pkmn from json and sets them as options in select input from html form.

--- Jour04/job03/script.js
+++ Jour04/job03/script.js
@@ -1,23 +1,74 @@
-/* Créez un formulaire permettant de trier ces données. 
-
-Il doit contenir les champs suivant : 
-
-id (input type text), 
-nom (input type text), 
-type (liste déroulante) 
-filtrer (input type button). 
-
+/*
 Lorsque l’on clique sur “filtrer”, le script doit à l’aide de Fetch, récupérer le contenu du fichier et lister les éléments répondant aux critères sélectionnés en les affichant sur une page 
 HTML. */
 
 
 document.addEventListener("DOMContentLoaded", () => {
+    
+    const select = document.querySelector('select');
+    
+    var pokemonType = [];
 
+    //permet d'afficher autant de balises "option" qu'il y a de types de pokemon dans le json
+    fetch('pokemon.json')
 
-    option = document.createElement ('option');
-    option.value = valeur;
-    select.appendChild(option);
-});
+        //teste si le fetch fonctionne puis retourne la promesse "response"
+        .then(response => {
 
-//spread
-//set
+            if (!response.ok) {
+                
+                throw new Error("HTTP error, status = " + response.status);
+
+            }
+
+            return response.json();
+        })
+
+        //insérer les types de pokemon dans un array
+        .then(data => {
+
+            for (pokemons of data) {
+
+                for (types of pokemons.type) {
+
+                    if (!pokemonType.includes(types)){
+
+                        pokemonType.push(types);
+
+                    }
+                }  
+            }
+        })
+
+        .then( event => {
+
+            for (const type of pokemonType) {
+                
+                option = document.createElement ('option');
+
+                option.value = type;
+
+                option.innerHTML = type;
+
+                select.appendChild(option);
+        
+            }
+
+        })
+
+        .catch(function(error) {
+
+            var p = document.createElement('p');
+
+            p.appendChild(
+
+                document.createTextNode('Error: ' + error.message)
+
+            );
+
+            document.body.insertBefore(p, select);
+            
+        })
+        //spread operator: (...)
+        //set
+})
